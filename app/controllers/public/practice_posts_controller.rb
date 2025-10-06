3cff6ab4a656ed8c594ee81b0737c0d44e11a975
tkitada <taka.lebron.2306@gmail.com>
2025-10-06 04:51:14 +0000

    Golf note(新規投稿ページ)のMVC作成

--- app/controllers/public/practice_posts_controller.rb
+++ app/controllers/public/practice_posts_controller.rb
@@ -6,10 +6,23 @@ class Public::PracticePostsController < ApplicationController
     @practice_post = PracticePost.new
   end
 
+  def create
+    @practice_post = PracticePost.new(practice_post_params)
+    @practice_post.user_id = current_user.id
+    @practice_post.save
+    redirect_to public_practice_posts_show_path
+  end
+
   def show
   end
 
   def edit
   end
-  
+
+  private
+
+  def practice_post_params
+    params.require(:practice_post).permit(:practice_date, :practice_focus, :shot_hits, :content)
+  end
+
 end
