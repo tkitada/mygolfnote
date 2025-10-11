d042bac77eb258fdd10679de24e41917aaebc086
tkitada <taka.lebron.2306@gmail.com>
2025-10-11 03:50:31 +0000

    開発環境でのテスト・不具合修正/実装フェーズ１

--- app/controllers/public/practice_posts_controller.rb
+++ app/controllers/public/practice_posts_controller.rb
@@ -1,6 +1,6 @@
 class Public::PracticePostsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :is_matching_login_user, only: [:edit, :update]
+  before_action :authenticate_user!, except: [:index]
+  before_action :set_practice_post, only: [:edit, :update, :destroy]
 
   def index
     @practice_posts = PracticePost.all
@@ -24,11 +24,9 @@ class Public::PracticePostsController < ApplicationController
   end
 
   def edit
-    @practice_post = current_user.practice_posts.find(params[:id])
   end
 
   def update
-    @practice_post = current_user.practice_posts.find(params[:id])
     if @practice_post.update(practice_post_params)
       redirect_to practice_post_path(@practice_post)
     else
@@ -37,18 +35,19 @@ class Public::PracticePostsController < ApplicationController
   end
 
   def destroy
-    @practice_post = current_user.practice_posts.find(params[:id])
     @practice_post.destroy
     redirect_to user_path(current_user)
   end
 
   private
 
-  def is_matching_login_user
-    unless @practice_post.user == current_user
+  def set_practice_post
+    @practice_post = current_user.practice_posts.find_by(id: params[:id])
+    unless @practice_post
       redirect_to root_path
     end
   end
+
 
   def practice_post_params
     params.require(:practice_post).permit(:practice_date, :practice_focus, :shot_hits, :content)
