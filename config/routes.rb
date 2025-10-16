2624acbb2914c4819c550281fd5731a7f756dd4a
Ubuntu <taka.lebron.2306@gmail.com>
2025-10-16 07:00:01 +0000

    [管理者機能/ユーザー機能]controller, routing作成

--- config/routes.rb
+++ config/routes.rb
@@ -25,5 +25,9 @@ Rails.application.routes.draw do
     end
     resources :users, only: [:index, :show, :edit, :update]
   end
+
+  namespace :admin do
+    resources :users, only: [:index, :show, :edit, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
