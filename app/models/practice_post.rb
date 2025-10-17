bc6cdb70336158914b41c02b5f494ec39c699130
Ubuntu <taka.lebron.2306@gmail.com>
2025-10-17 11:30:01 +0000

    [いいね機能]favoriteモデル作成・アソシエーション追加

--- app/models/practice_post.rb
+++ app/models/practice_post.rb
@@ -1,9 +1,14 @@
 class PracticePost < ApplicationRecord
   belongs_to :user
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_one_attached :post_image
   has_rich_text :content
 
   validates :practice_date, :content, presence: :true
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 
 end
