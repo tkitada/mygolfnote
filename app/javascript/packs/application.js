bbbdfc9b01b074bbb805b3f4b0c5168353dce71f
Ubuntu <taka.lebron.2306@gmail.com>
2025-10-23 15:03:26 +0000

    [レイアウト・デザイン調整]header, main-visual調整・レスポンジブ対応調整

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+const images = require.context('../images', true)
 
 import "jquery";
 import "popper.js";
 import "bootstrap";
-import "../stylesheets/application"; 
+import "../stylesheets/application.scss"; 
 
 // 作成したプロフィール画像ハンドラーをインポート
 import '../packs/profile_image_handler' 
