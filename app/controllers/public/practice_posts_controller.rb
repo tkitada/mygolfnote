6739600f1ef71331373b5f108710374c6ab99d98
Ubuntu <taka.lebron.2306@gmail.com>
2025-10-28 15:25:47 +0000

    [pagination]controller/view記述・実装完了

--- app/controllers/public/practice_posts_controller.rb
+++ app/controllers/public/practice_posts_controller.rb
@@ -5,7 +5,7 @@ class Public::PracticePostsController < ApplicationController
   def index
     @practice_posts = PracticePost.all
     @tag_list = Tag.all
-    @practice_posts = PracticePost.all.page(params[:id])
+    @practice_posts = PracticePost.all.page(params[:id]).per(5)
   end
 
   def new
