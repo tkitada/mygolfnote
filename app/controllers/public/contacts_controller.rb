8e748472c25e32f8885f4918767ae5d3e61265bd
Ubuntu <taka.lebron.2306@gmail.com>
2025-11-17 09:05:36 +0000

    不具合を修正・レイアウト調整

--- app/controllers/public/contacts_controller.rb
+++ app/controllers/public/contacts_controller.rb
@@ -7,7 +7,7 @@ class Public::ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     
     if @contact.save
-      ContactMailer.contact_email(@contact).deliver_later
+      ContactMailer.contact_mail(@contact).deliver_later
       redirect_to contact_path(@contact), notice: "お問い合わせを送信しました"
     else
       flash.now[:alert] = "入力内容に誤りがあります"
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ApplicationMailer
-  default to: "MAIL_ADDRESS"
+  default to: ENV["MAIL_ADDRESS"]
 
-  def send_mail(contact)
+  def contact_mail(contact)
     @contact = contact
     mail(
       from: @contact.email,
-      subject: '【お問い合わせ】' + @contact.subject_i18n
+      subject: '【お問い合わせ】' + I18n.t("activerecord.attributes.contact.subject.#{@contact.subject}") 
     )
   end
 end
