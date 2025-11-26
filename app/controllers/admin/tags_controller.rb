56b944976a093c8a9de6a1b90f132e4c50c7ff08
Ubuntu <taka.lebron.2306@gmail.com>
2025-11-26 06:51:57 +0000

    create, destoryアクション定義・バリデーション設定

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -6,7 +6,25 @@ class Admin::TagsController < ApplicationController
     @tag = Tag.new
   end
 
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to admin_tags_path, notice: "タグを作成しました"
+    else
+      @tags = Tag.all.order(created_at: :desc)
+      render :index
+    end
+  end
 
-  def edit
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    redirect_to admin_tags_path
+  end
+
+  private 
+
+  def tag_params
+    params.require(:tag).permit(:name)
   end
 end
